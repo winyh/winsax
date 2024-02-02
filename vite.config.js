34df41c5a48288fb866f6a45e68a755d55e2bdb7
winyh <1124692067@qq.com>
2024-02-02 20:18:32 +0800

    fix:gh-pages 404

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   build:{
-    assetsDir: 'winsax/assets', // 静态资源存放文件夹名
+    base: '/winsax/', // https://cn.vitejs.dev/guide/static-deploy#github-pages
   },
   plugins: [react()],
 })
