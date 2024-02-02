24988a695733290d583be3c1aee15e4914023673
winyh <1124692067@qq.com>
2024-02-02 19:41:04 +0800

    fix:自动化流程优化和静态资源路径修改

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    assetsDir: 'winsax/assets', // 静态资源存放文件夹名
+  },
   plugins: [react()],
 })
