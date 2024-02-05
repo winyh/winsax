f713cb92fdcf7d31cba8c7fe2788e6ed37596cef
winyh <winyh@users.noreply.github.com>
2024-02-05 09:57:16 +0000

    deploy: 31980ffba6b1827f1d8f397bfd93aaa3e06a1461

---
+++ assets/404-3BeEk8rv.js
@@ -0,0 +1,1 @@
+import{o,j as t,B as e}from"./index-qC7q3ryG.js";import{R as r}from"./index-2GOk0UM9.js";import"./ExclamationCircleFilled-TdwVV4WB.js";const m=()=>{const s=o(),a=()=>{s("/winsax/dashboard")};return t.jsx(r,{status:"404",title:"404",subTitle:"对不起, 您访问的页面不存在.",extra:t.jsx(e,{type:"primary",onClick:a,children:"返回首页"})})};export{m as default};
---
+++ assets/index-YXTnduEV.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-qC7q3ryG.js";import{C as e}from"./index-EPTWuJcQ.js";const s=()=>r.jsx(e,{title:"用户中心",bordered:!1,children:"这是用户信息"});export{s as default};
