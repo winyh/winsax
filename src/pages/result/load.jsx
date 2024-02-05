2856a35dc0e03796f00595bfb729548a56945d1d
winyh <1124692067@qq.com>
2024-02-05 18:22:51 +0800

    feat:资源加载和数据AP接口加载区分开

--- src/pages/result/load.jsx
+++ src/pages/result/load.jsx
@@ -1,15 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Spin } from "antd"
 import { useNavigate } from "react-router-dom"
 import "./index.less"
 const Loading = () => {
-  const [spinning, setSpinning] = React.useState(false)
-  const showLoader = () => {
-    setSpinning(true)
-    setTimeout(() => {
-      setSpinning(false)
-    }, 3000)
-  }
-  return <Spin spinning={spinning} tip="拼命加载中..." fullscreen />
+  const [spinning, setSpinning] = useState(true)
+  return <Spin spinning={spinning} tip="加载资源中..." fullscreen />
 }
 export default Loading
